2bb36d3ff7bf7eb24dd77ab363c55b603503bfb9
Yasir Ali <sdr.yasir@gmail.com>
2021-09-07 19:23:40 +0500

    I have changed the class name inside contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div id="abc">
+        <div id="test">
             <h1>This is Contact Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sit sequi id fugiat repellat eum error. Ea voluptatum quis aliquam aut assumenda eligendi consequatur error nostrum, earum, officiis pariatur nihil magni hic libero eius tempore nesciunt illum veritatis, nam architecto quae optio. Eos obcaecati blanditiis facilis sunt maiores commodi! Labore quibusdam eos placeat quas, obcaecati doloribus aut veniam quasi consequatur dignissimos est! Rerum quidem voluptatum optio earum recusandae dolores esse, hic odit architecto veritatis veniam eligendi. Reprehenderit doloribus voluptates magnam maxime, eveniet id eum facilis aliquam ducimus architecto libero est veniam a in recusandae nostrum saepe, deserunt cum asperiores? Architecto.</p>
         </div>
